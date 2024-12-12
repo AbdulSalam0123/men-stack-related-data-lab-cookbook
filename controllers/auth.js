a0142dae817dcbb3ed5733b76b1299f4d1506a92
Abdulsalam20000 <20193925@stu.uob.edu.bh>
2024-12-12 20:26:44 +0300

    done

---
+++ controllers/auth.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+router.get('/sign-up', (req, res) =>
+{
+  res.render('auth/sign-up.ejs')
+});
+
+router.post('/sign-up', async (req, res) =>
+{
+
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) 
+    {
+      return res.render('auth/sign-up.ejs');
+    }
+
+    if (req.body.password !== req.body.confirmPassword) 
+    {
+      return res.render('auth/sign-up.ejs');
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User(
+    {
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.redirect('/auth/sign-in');
+});
+
+router.get('/sign-in', (req, res) =>
+{
+  res.render('auth/sign-in.ejs');
+});
+
+router.post('/sign-in', async (req, res) =>
+{
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) 
+    {
+      return res.render('auth/sign-in.ejs');
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, userInDatabase.password);
+    if (!validPassword) 
+        {
+      return res.render('auth/sign-in.ejs');
+    }
+
+    req.session.user =
+    {
+      username: userInDatabase.username,
+      _id: userInDatabase._id,
+    };
+    res.redirect('/');
+});
+
+router.get('/sign-out', (req, res) => 
+{
+  req.session.destroy(() =>
+  {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,57 @@
+const dotenv= require('dotenv');
+dotenv.config();
+const express = require("express");
+const app= express();
+
+const methodOverride=require("method-override");
+
+const session = require("express-session");
+const passUserToView = require("./middleware/path-user-to-view");
+const router = require('express').Router();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+
+const port = process.env.port? process.env.port: "4000";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", ()=>
+{
+    console.log("Connected to MongoDB: ", mongoose.connection.name);
+});
+
+//middleware
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized: true
+}))
+
+
+//Require controllers
+const authCtrl = require("./controllers/auth");
+const recipesCtrl = require('./controllers/recipes.js');
+const ingredientsCtrl = require('./controllers/ingredients.js');
+const isSignedIn = require('./middleware/is-signed-in');
+
+
+app.use(passUserToView);
+app.use("/auth", authCtrl);
+app.use('/recipes', recipesCtrl);
+app.use('/ingredients', ingredientsCtrl);
+
+//port route
+app.get('/', async (req,res)=>
+{
+    res.render("index.ejs");
+})
+
+app.listen(port, ()=>
+{
+    console.log("Listening on port:", port);
+})
+module.exports = router;
